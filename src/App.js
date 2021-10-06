ce35eed5387ee9e2009aa38e08e8662821924119
bukharov <dilmurod.bukharov@gmail.com>
2021-10-06 10:59:19 -0400

    Tasks are deletable and made it to toggle

--- src/App.js
+++ src/App.js
@@ -23,10 +23,29 @@ function App() {
       reminder: true,
     },
   ]);
+
+  // deleting a task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,9 @@
-const Tasks = ({ tasks }) => {
+import Task from "./Task";
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task task={task} onDelete={onDelete} onToggle={onToggle} />
       ))}
     </>
   );
